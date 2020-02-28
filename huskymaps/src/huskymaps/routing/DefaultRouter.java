953ad636dc4d30d8ead9171675dded554f1a1110
haokun <megakbig@gmail.com>
2020-02-28 06:17:00 -0800

    everything done

--- huskymaps/src/huskymaps/routing/DefaultRouter.java
+++ huskymaps/src/huskymaps/routing/DefaultRouter.java
@@ -3,6 +3,7 @@ package huskymaps.routing;
 import graphpathfinding.AStarGraph;
 import graphpathfinding.AStarPathFinder;
 import graphpathfinding.ShortestPathFinder;
+import graphpathfinding.ShortestPathResult;
 import huskymaps.graph.Coordinate;
 import huskymaps.graph.Node;
 import huskymaps.graph.StreetMapGraph;
@@ -10,6 +11,8 @@ import pointsets.KDTreePointSet;
 import pointsets.Point;
 import pointsets.PointSet;
 
+import java.time.Duration;
+import java.util.LinkedList;
 import java.util.List;
 
 import static huskymaps.utils.Spatial.projectToPoint;
@@ -19,10 +22,21 @@ import static huskymaps.utils.Spatial.projectToPoint;
  */
 public class DefaultRouter extends Router {
     private StreetMapGraph graph;
+    private PointSet pointSet;
+    private ShortestPathFinder finder;
 
     public DefaultRouter(StreetMapGraph graph) {
         this.graph = graph;
-        // TODO: replace this with your code
+        List<NodePoint> nodePoints = new LinkedList<>();
+
+        for (Node vertex : graph.allNodes()) {
+            if (!graph.neighbors(vertex).isEmpty()) {
+                nodePoints.add(createNodePoint(vertex));
+            }
+        }
+
+        pointSet = createPointSet(nodePoints);
+        finder = createPathFinder(graph);
     }
 
     @Override
@@ -46,20 +60,22 @@ public class DefaultRouter extends Router {
     protected Node closest(Coordinate c) {
         // Project to x and y coordinates instead of using raw lat and lon for finding closest points:
         Point p = projectToPoint(c, Point::new);
-        // TODO: replace this with your code
-        return null;
+        NodePoint result = (NodePoint) pointSet.nearest(p);
+        return result.node();
     }
 
     @Override
     public List<Node> shortestPath(Coordinate start, Coordinate end) {
         Node src = closest(start);
         Node dest = closest(end);
-        // TODO: replace this with your code
+        Duration time = Duration.ofSeconds(30);
+        ShortestPathResult<Node> path = finder.findShortestPath(src, dest, time);
         /*
         Feel free to use any arbitrary duration for your path finding timeout; we don't expect
         queries to take more than a few seconds, so e.g. 10-30 seconds is pretty reasonable.
          */
-        return List.of();
+
+        return path.solution();
     }
 
     @Override
--- huskymaps/src/huskymaps/searching/DefaultSearcher.java
+++ huskymaps/src/huskymaps/searching/DefaultSearcher.java
@@ -6,14 +6,47 @@ import autocomplete.Term;
 import huskymaps.graph.Node;
 import huskymaps.graph.StreetMapGraph;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * @see Searcher
  */
 public class DefaultSearcher extends Searcher {
+    private final Autocomplete searcherUsed;
+    private final HashMap<String, List<Node>> allLocations;
+
     public DefaultSearcher(StreetMapGraph graph) {
-        // TODO: replace this with your code
+        allLocations = new HashMap<>();
+        List<Term> termList = new LinkedList<>();
+
+        for (Node vertex : graph.allNodes()) {
+            if (vertex.name() != null) {
+                String name = vertex.name();
+                int weight = vertex.importance();
+
+                Term newTerm = createTerm(name, weight);
+                termList.add(newTerm);
+                if (!allLocations.containsKey(name)) {
+                    List<Node> nodes = new ArrayList<>();
+                    allLocations.put(name, nodes);
+                }
+                allLocations.get(name).add(vertex);
+
+            }
+        }
+        Term[] termsArray = new DefaultTerm[termList.size()];
+        for (int i = 0; i < termList.size(); i++) {
+            termsArray[i] = termList.get(i);
+        }
+
+        searcherUsed = createAutocomplete(termsArray);
+
+
     }
 
     @Override
@@ -28,13 +61,24 @@ public class DefaultSearcher extends Searcher {
 
     @Override
     public List<String> getLocationsByPrefix(String prefix) {
-        // TODO: replace this with your code
-        return List.of();
+        Term[] result = searcherUsed.findMatchesForPrefix(prefix);
+        HashSet<String> matchSet = new HashSet<>();
+        List<String> matches = new ArrayList<>();
+
+        for (int i = result.length - 1; i >= 0; i--) {
+            if (result[i].query() != null) {
+                matchSet.add(result[i].query());
+            }
+        }
+
+        matches.addAll(matchSet);
+
+        return matches;
     }
 
     @Override
     public List<Node> getLocations(String locationName) {
-        // TODO: replace this with your code
-        return List.of();
+        List<Node> result = allLocations.get(locationName);
+        return result;
     }
 }
